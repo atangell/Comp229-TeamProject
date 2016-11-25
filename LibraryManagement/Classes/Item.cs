2cd7c48122a4c738a6b3db90b658c155c20881cd
atangell <amare@DESKTOP-T5LGITB>
2016-11-25 18:21:51 -0500

    Fifth Commit - Amar

---
+++ LibraryManagement/Classes/Item.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LibraryManagement.Classes
+{
+    public class Item
+    {
+        public int ItemId { get; set; }
+        public string ItemType { get; set; }
+        public string Name { get; set; }
+        public string ShortDesc { get; set; }
+        public string AuthorName { get; set; }
+        public string PublisherName { get; set; }
+        public bool IsCompleted { get; set; }
+        public string ItemStatus { get; set; }
+        public string Link { get; set; }
+        public Double ReviewScore { get; set; }
+        public DateTime DateItemAdded { get; set; }
+        public string IsbnUpc { get; set; }
+        public string ItemPlatform { get; set; }
+    }
+}

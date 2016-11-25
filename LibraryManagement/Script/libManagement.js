8fde7196a79712a788c31034d4af17fab5913afc
atangell <amare@DESKTOP-T5LGITB>
2016-11-25 07:30:56 -0500

    Fourth Commit-Amar

---
+++ LibraryManagement/Script/libManagement.js
@@ -0,0 +1,14 @@
+﻿var libManagement = {
+
+
+    init: function () {
+        var windowHeight = $(window).height();
+        var wrapperHeight = $('.wrapper').height();
+        if (windowHeight > wrapperHeight)
+            $('.wrapper').height(windowHeight);
+    }
+}
+
+$(document).ready(function () {
+    libManagement.init();
+});

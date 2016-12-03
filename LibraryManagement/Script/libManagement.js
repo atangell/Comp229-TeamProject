7770559a871989ff826a2e439856f0e42a711c18
Balaji <balajiprasad2583@gmail.com>
2016-12-02 19:34:31 -0500

    v0.8-modifiedLibManagement.js  for pop up

--- LibraryManagement/Script/libManagement.js
+++ LibraryManagement/Script/libManagement.js
@@ -1,7 +1,14 @@
 ﻿var libManagement = {
+    closePopup: function () {
+        $(".overlay-div").hide("slow");
+    },
+    openPopup: function () {
 
+        $(".overlay-div").show("slow");
+    },
 
     init: function () {
+        var isUserExists = $('.isUserExists').text();
         var windowHeight = $(window).height();
         var wrapperHeight = $('.wrapper').height();
         if (windowHeight > wrapperHeight)
@@ -11,4 +18,8 @@
 
 $(document).ready(function () {
     libManagement.init();
+    $('#imgPopupClose, .btnCancel').on('click', function () {
+        libManagement.closePopup();
+
+    });
 });
